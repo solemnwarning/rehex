fd8268c31e536060ddd8375dfc3f36a9270caaf0
Daniel Collins <solemnwarning@solemnwarning.net>
2020-08-06 13:37:55 +0100

    Generate msvc_version.h as part of main rehex project.

--- msvc/gen_content.bat
+++ msvc/gen_content.bat
@@ -1,13 +1,10 @@
 setlocal enabledelayedexpansion
 
-set tool_exe="%1"
-
 cd %~dp0
 cd ..
 cd res
 
-git log -1 --format="#define LONG_VERSION \"%%H\"" > version_msvc.h
+git log -1 --format="#define LONG_VERSION \"Snapshot %%H\"" > version_msvc.h
 if %errorlevel% neq 0 exit /b %errorlevel%
-
 
 exit /b 0

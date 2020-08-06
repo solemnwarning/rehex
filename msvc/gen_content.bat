8c7b8386d37f0493fa18ff7bf143906b3b4cae57
Daniel Collins <solemnwarning@solemnwarning.net>
2020-08-06 12:50:54 +0100

    Prepare embedded resources as Visual Studio build steps
    Adds a 'resources' project which has all the resource files as its
    sources with the .c/.h files as their targets. Keeps VS aware of the
    dependencies and will allow (re)building them automatically as
    appropriate.

--- msvc/gen_content.bat
+++ msvc/gen_content.bat
@@ -6,15 +6,6 @@ cd %~dp0
 cd ..
 cd res
 
-%tool_exe% ../LICENSE.txt LICENSE_TXT license.c license.h
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-for %%f in (*.png) do (
-    set fname=%%~nf
-    %tool_exe% !fname!.png !fname!_png !fname!.c !fname!.h
-    if %errorlevel% neq 0 exit /b %errorlevel%
-)
-
 git log -1 --format="#define LONG_VERSION \"%%H\"" > version_msvc.h
 if %errorlevel% neq 0 exit /b %errorlevel%
 

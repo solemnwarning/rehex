solemnwarning/rehex

commit 0e4245c941905ce4cd6acc6643bb68c0570e7c06

Prefix environment LIBS to our own so -L paths can be specified there.

Makefile
@@ -33,7 +33,7 @@ WX_LIBS     := $(call shell-or-die,$(WX_CONFIG) --libs     base core aui propgri
 CFLAGS   := -Wall -std=c99   -ggdb -I. -Iinclude/ $(CFLAGS)
 CXXFLAGS := -Wall -std=c++11 -ggdb -I. -Iinclude/ $(WX_CXXFLAGS) $(CXXFLAGS)
 
-LIBS := $(WX_LIBS) -ljansson -lcapstone $(LIBS)
+LIBS := $(LIBS) $(WX_LIBS) -ljansson -lcapstone
 
 ifeq ($(DEBUG),)
 	DEBUG=0

commit 7781e199d4d0b4eddc1dca0580416abe93adb90a

Split LIBS into LDLIBS and LDFLAGS
Commit 0e4245c941905ce4cd6acc6643bb68c0570e7c06 broke the static link
flags set by Makefile.win, add separate LDLIBS and LDFLAGS so we can
work around the idiosyncracies of ld.

Makefile
@@ -33,7 +33,7 @@ WX_LIBS     := $(call shell-or-die,$(WX_CONFIG) --libs     base core aui propgri
 CFLAGS   := -Wall -std=c99   -ggdb -I. -Iinclude/ $(CFLAGS)
 CXXFLAGS := -Wall -std=c++11 -ggdb -I. -Iinclude/ $(WX_CXXFLAGS) $(CXXFLAGS)
 
-LIBS := $(LIBS) $(WX_LIBS) -ljansson -lcapstone
+LDLIBS := $(WX_LIBS) -ljansson -lcapstone $(LDLIBS)
 
 ifeq ($(DEBUG),)
 	DEBUG=0
@@ -143,7 +143,7 @@ APP_OBJS := \
 
 $(EXE): $(APP_OBJS)
 	$(CXX) $(CXXFLAGS) -DLONG_VERSION='"$(LONG_VERSION)"' -c -o res/version.o res/version.cpp
-	$(CXX) $(CXXFLAGS) -o $@ $^ res/version.o $(LIBS)
+	$(CXX) $(CXXFLAGS) -o $@ $^ res/version.o $(LDFLAGS) $(LDLIBS)
 
 TEST_OBJS := \
 	googletest/src/gtest-all.o \
@@ -191,7 +191,7 @@ TEST_OBJS := \
 	tests/util.o
 
 tests/all-tests: $(TEST_OBJS)
-	$(CXX) $(CXXFLAGS) -o $@ $^ $(LIBS)
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(LDFLAGS) $(LDLIBS)
 
 $(EMBED_EXE): tools/embed.cpp
 	$(CXX) $(CXXFLAGS) -o $@ $<

Makefile.win
@@ -16,7 +16,7 @@
 
 EXE            := rehex.exe
 EXTRA_APP_OBJS := res/winres.o
-LIBS           += -Wl,-Bstatic -lstdc++ -lwinpthread -Wl,-Bdynamic -static-libgcc -static-libstdc++
+LDLIBS         += -Wl,-Bstatic -lstdc++ -lwinpthread -Wl,-Bdynamic -static-libgcc -static-libstdc++
 EMBED_EXE      := tools/embed.exe
 
 # Needed for PRIxNN macros from inttypes.h under MinGW
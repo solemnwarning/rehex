9fd37435fc80fd32e355960920cb34b6cb8804ee
Daniel Collins <solemnwarning@solemnwarning.net>
2020-12-20 10:55:03 +0000

    Tidy up LuaPluginLoader and fix memory leak.

--- src/LuaPluginLoader.cpp
+++ src/LuaPluginLoader.cpp
@@ -1,7 +1,25 @@
+/* Reverse Engineer's Hex Editor
+ * Copyright (C) 2020 Daniel Collins <solemnwarning@solemnwarning.net>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #include "platform.hpp"
 
 #include <assert.h>
-#include <list>
+#include <exception>
+#include <stdexcept>
 #include <string>
 #include <wx/dir.h>
 #include <wx/event.h>
@@ -16,12 +34,33 @@
 #include "app.hpp"
 #include "lua-bindings/rehex_bind.h"
 #include "lua-plugin-preload.h"
-#include "mainwindow.hpp"
+#include "LuaPluginLoader.hpp"
 
-static std::list<wxLuaState> states;
+static REHex::App::SetupHookRegistration load_lua_plugins_hook(
+	REHex::App::SetupPhase::READY,
+	&REHex::LuaPluginLoader::load_all_plugins);
 
-static void load_lua_plugins()
+std::unique_ptr<wxEvtHandler> REHex::LuaPluginLoader::default_handler;
+std::list<REHex::LuaPlugin> REHex::LuaPluginLoader::loaded_plugins;
+
+void REHex::LuaPluginLoader::load_all_plugins()
 {
+	static bool is_first_call = true;
+	assert(is_first_call);
+	is_first_call = false;
+	
+	default_handler.reset(new wxEvtHandler);
+	
+	default_handler->Bind(wxEVT_LUA_ERROR, [&](wxLuaEvent &event)
+	{
+		fprintf(stderr, "wxEVT_LUA_ERROR: %s\n", event.GetString().mb_str().data());
+	});
+	
+	default_handler->Bind(wxEVT_LUA_PRINT, [&](wxLuaEvent &event)
+	{
+		fprintf(stderr, "wxEVT_LUA_PRINT: %s\n", event.GetString().mb_str().data());
+	});
+	
 	wxDir dir("./");
 	wxString filename;
 	
@@ -41,35 +80,49 @@ static void load_lua_plugins()
 		{
 			wxFileName file_path(dir.GetName(), filename);
 			
-			wxEvtHandler *eh = new wxEvtHandler;
-			
-			eh->Bind(wxEVT_LUA_ERROR, [&](wxLuaEvent &event)
-			{
-				fprintf(stderr, "wxEVT_LUA_ERROR: %s\n", event.GetString().mb_str().data());
-				return;
-			});
-			
-			wxLuaState s(eh);
-			
-			int run_state = s.RunBuffer((const char*)(LUA_PLUGIN_PRELOAD), sizeof(LUA_PLUGIN_PRELOAD), "lua-plugin-preload.lua");
-			if(run_state == 0)
-			{
-				run_state = s.RunFile(file_path.GetFullPath());
+			try {
+				loaded_plugins.push_back(load_plugin(file_path.GetFullPath().ToStdString()));
 			}
-			
-			if(run_state == 0)
+			catch(const std::exception &e)
 			{
-				states.push_back(s);
-			}
-			else{
-				fprintf(stderr, "Failed to load plugin %s: %s\n",
-					filename.mb_str().data(),
-					wxlua_LUA_ERR_msg(run_state).mb_str().data());
+				fprintf(stderr, "====\nFailed to load plugin %s\n\n%s\n====\n", filename.mb_str().data(), e.what());
 			}
 		} while (dir.GetNext(&filename));
 	}
 }
 
-static REHex::App::SetupHookRegistration load_lua_plugins_hook(
-	REHex::App::SetupPhase::READY,
-	&load_lua_plugins);
+REHex::LuaPlugin REHex::LuaPluginLoader::load_plugin(const std::string &filename)
+{
+	wxEvtHandler local_handler;
+	std::string output;
+	
+	local_handler.Bind(wxEVT_LUA_ERROR, [&](wxLuaEvent &event)
+	{
+		output += event.GetString().ToStdString() + "\n";
+	});
+	
+	local_handler.Bind(wxEVT_LUA_PRINT, [&](wxLuaEvent &event)
+	{
+		output += event.GetString().ToStdString() + "\n";
+	});
+	
+	wxLuaState s(&local_handler);
+	
+	int run_state = s.RunBuffer((const char*)(LUA_PLUGIN_PRELOAD), sizeof(LUA_PLUGIN_PRELOAD), "lua-plugin-preload.lua");
+	if(run_state == 0)
+	{
+		run_state = s.RunFile(filename);
+	}
+	
+	if(run_state != 0)
+	{
+		throw std::runtime_error(output + wxlua_LUA_ERR_msg(run_state).ToStdString());
+	}
+	
+	s.SetEventHandler(default_handler.get());
+	printf("%s", output.c_str());
+	
+	return LuaPlugin(s);
+}
+
+REHex::LuaPlugin::LuaPlugin(const wxLuaState &lua): lua(lua) {}
---
+++ src/LuaPluginLoader.hpp
@@ -0,0 +1,50 @@
+/* Reverse Engineer's Hex Editor
+ * Copyright (C) 2020 Daniel Collins <solemnwarning@solemnwarning.net>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef REHEX_PLUGINLOADER_HPP
+#define REHEX_PLUGINLOADER_HPP
+
+#include <list>
+#include <memory>
+#include <string>
+#include <wx/event.h>
+#include <wxlua/wxlua.h>
+
+namespace REHex
+{
+	class LuaPlugin
+	{
+		public:
+			LuaPlugin(const wxLuaState &lua);
+			
+		private:
+			wxLuaState lua;
+	};
+	
+	class LuaPluginLoader
+	{
+		private:
+			static std::unique_ptr<wxEvtHandler> default_handler;
+			static std::list<LuaPlugin> loaded_plugins;
+			
+		public:
+			static void load_all_plugins();
+			static LuaPlugin load_plugin(const std::string &filename);
+	};
+}
+
+#endif /* !REHEX_PLUGINLOADER_HPP */

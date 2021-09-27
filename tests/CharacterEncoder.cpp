dc843dd28edd4dcfcae4b5fa640e94ad989c0efe
Daniel Collins <solemnwarning@solemnwarning.net>
2021-09-27 23:53:36 +0100

    Add some UTF-8 character decode tests.

--- tests/CharacterEncoder.cpp
+++ tests/CharacterEncoder.cpp
@@ -76,7 +76,7 @@ TEST(CharacterEncoderASCII, Decode)
 
 TEST(CharacterEncoder88591, Decode)
 {
-	CharacterEncoderIconv encoder("ISO-8859-1");
+	CharacterEncoderIconv encoder("ISO-8859-1", 1);
 	
 	TEST_VALID_DECODE( X( '\0' ), X( '\0' ), X( '\0' ), "Control characters are decoded" );
 	TEST_VALID_DECODE( X( '\n' ), X( '\n' ), X( '\n' ), "Control characters are decoded" );
@@ -93,4 +93,43 @@ TEST(CharacterEncoder88591, Decode)
 	TEST_VALID_DECODE( X( '\r', '\n' ), X( '\r' ), X( '\r'       ), "Trailing characters are ignored" );
 	TEST_VALID_DECODE( X( 'Z', 'X'   ), X( 'Z'  ), X( 'Z'        ), "Trailing characters are ignored" );
 	TEST_VALID_DECODE( X( 0xA1, '!'  ), X( 0xA1 ), X( 0xC2, 0xA1 ), "Trailing characters are ignored" );
+}
+
+TEST(CharacterEncoderUTF8, Decode)
+{
+	CharacterEncoderIconv encoder("UTF-8", 1);
+	
+	TEST_VALID_DECODE( X( '\0' ), X( '\0' ), X( '\0' ), "Control characters are decoded" );
+	TEST_VALID_DECODE( X( '\n' ), X( '\n' ), X( '\n' ), "Control characters are decoded" );
+	TEST_VALID_DECODE( X( 0x7F ), X( 0x7F ), X( 0x7F ), "Control characters are decoded" ); /* DEL */
+	
+	TEST_VALID_DECODE( X( 'A' ), X( 'A' ), X( 'A' ), "7-bit characters are decoded" );
+	TEST_VALID_DECODE( X( '9' ), X( '9' ), X( '9' ), "7-bit characters are decoded" );
+	TEST_VALID_DECODE( X( '#' ), X( '#' ), X( '#' ), "7-bit characters are decoded" );
+	
+	TEST_VALID_DECODE( X( 0xC2, 0x80             ), X( 0xC2, 0x80             ), X( 0xC2, 0x80             ), "2 byte codepoints are decoded" );
+	TEST_VALID_DECODE( X( 0xDF, 0xBF             ), X( 0xDF, 0xBF             ), X( 0xDF, 0xBF             ), "2 byte codepoints are decoded" );
+	TEST_VALID_DECODE( X( 0xE0, 0xA0, 0x80       ), X( 0xE0, 0xA0, 0x80       ), X( 0xE0, 0xA0, 0x80       ), "3 byte codepoints are decoded" );
+	TEST_VALID_DECODE( X( 0xEF, 0xBF, 0xBF       ), X( 0xEF, 0xBF, 0xBF       ), X( 0xEF, 0xBF, 0xBF       ), "3 byte codepoints are decoded" );
+	TEST_VALID_DECODE( X( 0xF0, 0x90, 0x80, 0x80 ), X( 0xF0, 0x90, 0x80, 0x80 ), X( 0xF0, 0x90, 0x80, 0x80 ), "4 byte codepoints are decoded" );
+	TEST_VALID_DECODE( X( 0xF0, 0xA0, 0x80, 0x80 ), X( 0xF0, 0xA0, 0x80, 0x80 ), X( 0xF0, 0xA0, 0x80, 0x80 ), "4 byte codepoints are decoded" );
+	
+	TEST_INVALID_DECODE( X( 0x80, 0xC2             ), "misaligned 2 byte codepoints are rejected" );
+	TEST_INVALID_DECODE( X( 0xBF, 0xDF             ), "misaligned 2 byte codepoints are rejected" );
+	TEST_INVALID_DECODE( X( 0xA0, 0x80, 0xE0       ), "misaligned 3 byte codepoints are rejected" );
+	TEST_INVALID_DECODE( X( 0xBF, 0xEF, 0xBF       ), "misaligned 3 byte codepoints are rejected" );
+	TEST_INVALID_DECODE( X( 0x80, 0x80, 0xF0, 0x90 ), "misaligned 4 byte codepoints are rejected" );
+	TEST_INVALID_DECODE( X( 0x80, 0xF0, 0xA0, 0x80 ), "misaligned 4 byte codepoints are rejected" );
+	
+	TEST_VALID_DECODE( X( '\r', '\n'                  ), X( '\r'                   ), X( '\r'                   ), "Trailing characters are ignored" );
+	TEST_VALID_DECODE( X( 'Z', 'X'                    ), X( 'Z'                    ), X( 'Z'                    ), "Trailing characters are ignored" );
+	TEST_VALID_DECODE( X( 0xC2, 0x80, 'Q'             ), X( 0xC2, 0x80             ), X( 0xC2, 0x80             ), "Trailing characters are ignored" );
+	TEST_VALID_DECODE( X( 0xEF, 0xBF, 0xBF, '0'       ), X( 0xEF, 0xBF, 0xBF       ), X( 0xEF, 0xBF, 0xBF       ), "Trailing characters are ignored" );
+	TEST_VALID_DECODE( X( 0xF0, 0xA0, 0x80, 0x80, 'A' ), X( 0xF0, 0xA0, 0x80, 0x80 ), X( 0xF0, 0xA0, 0x80, 0x80 ), "Trailing characters are ignored" );
+	
+	TEST_INVALID_DECODE( X( 0xC0, 0xAF                         ), "Overlong sequences are rejected" );
+	TEST_INVALID_DECODE( X( 0xE0, 0x80, 0xAF                   ), "Overlong sequences are rejected" );
+	TEST_INVALID_DECODE( X( 0xF0, 0x80, 0x80, 0xAF             ), "Overlong sequences are rejected" );
+	TEST_INVALID_DECODE( X( 0xF8, 0x80, 0x80, 0xAF             ), "Overlong sequences are rejected" );
+	TEST_INVALID_DECODE( X( 0xFC, 0x80, 0x80, 0x80, 0x80, 0xAF ), "Overlong sequences are rejected" );
 }

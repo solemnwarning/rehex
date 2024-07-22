68f58f7b1025f499b8e4c824b7f7b3ebbb2d29e9
Daniel Collins <solemnwarning@solemnwarning.net>
2024-07-22 21:48:13 +0100

    Add fixed version placeholders for MSVC builds.

--- msvc/gen_content.bat
+++ msvc/gen_content.bat
@@ -3,6 +3,10 @@ setlocal enabledelayedexpansion
 cd %~dp0
 cd ..
 
+rem Uncomment these lines in favour of the Git snapshot variants when tagging a release.
+rem echo #define LONG_VERSION "Version x.y.z" > res\version_msvc.h
+rem echo #define SHORT_VERSION "x.y.z" >> res\version_msvc.h
+
 git log -1 --format="#define LONG_VERSION \"Snapshot %%H\"" > res\version_msvc.h
 if %errorlevel% neq 0 exit /b %errorlevel%
 

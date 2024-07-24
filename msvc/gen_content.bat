256f3e7af724f46aafddb6fe80c2c12891a10af9
Daniel Collins <solemnwarning@solemnwarning.net>
2024-07-24 21:31:16 +0100

    Release 0.62.1

--- msvc/gen_content.bat
+++ msvc/gen_content.bat
@@ -3,14 +3,7 @@ setlocal enabledelayedexpansion
 cd %~dp0
 cd ..
 
-rem Uncomment these lines in favour of the Git snapshot variants when tagging a release.
-rem echo #define LONG_VERSION "Version x.y.z" > res\version_msvc.h
-rem echo #define SHORT_VERSION "x.y.z" >> res\version_msvc.h
-
-git log -1 --format="#define LONG_VERSION \"Snapshot %%H\"" > res\version_msvc.h
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-git log -1 --format="#define SHORT_VERSION \"%%H\"" >> res\version_msvc.h
-if %errorlevel% neq 0 exit /b %errorlevel%
+echo #define LONG_VERSION "Version 0.62.1" > res\version_msvc.h
+echo #define SHORT_VERSION "0.62.1" >> res\version_msvc.h
 
 exit /b 0

32a9de63866d2ad64599cd6fab24385fc150a566
Daniel Collins <solemnwarning@solemnwarning.net>
2025-03-12 22:19:59 +0000

    Pin Windows SDK to version 10.0.22621.0 for x86.
    Windows SDK version 10.26100.0 breaks backwards compatbility to XP
    using EnlyzeWinCompatLib, so just pin the old version for now.

--- msvc/vcpkg-deploy/triplets/x86-windows-static-enlyze.cmake
+++ msvc/vcpkg-deploy/triplets/x86-windows-static-enlyze.cmake
@@ -5,16 +5,33 @@ set(VCPKG_LIBRARY_LINKAGE static)
 
 STRING(REPLACE "\\" "/" SOLUTION_DIR $ENV{REHEX_SOLUTION_DIR})
 
+# Windows SDK header and library paths for the SDK version chosen in the project override the
+# latest installed version selected by vcpkg.
+
+execute_process(
+    COMMAND CMD /c "$ENV{REHEX_SOLUTION_DIR}\\Release\\format-paths.exe" ";" "$ENV{REHEX_WINSDK_INCLUDES}" " " "/I"
+    OUTPUT_VARIABLE WINSDK_INCLUDE_FLAGS
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    COMMAND_ERROR_IS_FATAL ANY)
+
+execute_process(
+    COMMAND CMD /c "$ENV{REHEX_SOLUTION_DIR}\\Release\\format-paths.exe" ";" "$ENV{REHEX_WINSDK_LIBDIRS}" " " "/LIBPATH:"
+    OUTPUT_VARIABLE WINSDK_LIBDIR_FLAGS
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    COMMAND_ERROR_IS_FATAL ANY)
+
 string(CONCAT VCPKG_LINKER_FLAGS_RELEASE
     "/LIBPATH:\"${SOLUTION_DIR}/EnlyzeWinCompatLib/EnlyzeWinCompatLib/build/Release/EnlyzeWinCompatLib/bin\""
     " /LIBPATH:\"${SOLUTION_DIR}/EnlyzeWinCompatLib/EnlyzeWinCompatLib/build/Release/winpthreads/bin\""
     " /LIBPATH:\"${SOLUTION_DIR}/EnlyzeWinCompatLib/EnlyzeWinCompatLib/build/Release/libc++/bin\""
+    " ${WINSDK_LIBDIR_FLAGS}"
     " libc++.lib winpthreads.lib libcpmt.lib")
 
 string(CONCAT VCPKG_LINKER_FLAGS_DEBUG
     "/LIBPATH:\"${SOLUTION_DIR}/EnlyzeWinCompatLib/EnlyzeWinCompatLib/build/Debug/EnlyzeWinCompatLib/bin\""
     " /LIBPATH:\"${SOLUTION_DIR}/EnlyzeWinCompatLib/EnlyzeWinCompatLib/build/Debug/winpthreads/bin\""
     " /LIBPATH:\"${SOLUTION_DIR}/EnlyzeWinCompatLib/EnlyzeWinCompatLib/build/Debug/libc++/bin\""
+    " ${WINSDK_LIBDIR_FLAGS}"
     " libc++.lib winpthreads.lib libcpmtd.lib")
 
 ## Toolchain setup
@@ -31,8 +48,19 @@ set(VCPKG_QT_TARGET_MKSPEC win32-clang-msvc) # For Qt5
 set(VCPKG_POLICY_SKIP_ARCHITECTURE_CHECK enabled)
 set(VCPKG_POLICY_SKIP_DUMPBIN_CHECKS enabled)
 
-set(VCPKG_C_FLAGS "/I\"${SOLUTION_DIR}/EnlyzeWinCompatLib/include\" /I\"${SOLUTION_DIR}/EnlyzeWinCompatLib/EnlyzeWinCompatLib/src/libcxx/src/winpthreads/include\"")
-set(VCPKG_CXX_FLAGS "/I\"${SOLUTION_DIR}/EnlyzeWinCompatLib/include\" /I\"${SOLUTION_DIR}/EnlyzeWinCompatLib/EnlyzeWinCompatLib/src/libcxx/include\" /I\"${SOLUTION_DIR}/EnlyzeWinCompatLib/EnlyzeWinCompatLib/src/libcxx/src/winpthreads/include\"")
+string(CONCAT VCPKG_C_FLAGS
+    "/I\"${SOLUTION_DIR}/EnlyzeWinCompatLib/include\""
+    " /I\"${SOLUTION_DIR}/EnlyzeWinCompatLib/EnlyzeWinCompatLib/src/libcxx/src/winpthreads/include\""
+    " ${WINSDK_INCLUDE_FLAGS}")
+
+string(CONCAT VCPKG_CXX_FLAGS
+    "/I\"${SOLUTION_DIR}/EnlyzeWinCompatLib/include\""
+    " /I\"${SOLUTION_DIR}/EnlyzeWinCompatLib/EnlyzeWinCompatLib/src/libcxx/include\""
+    " /I\"${SOLUTION_DIR}/EnlyzeWinCompatLib/EnlyzeWinCompatLib/src/libcxx/src/winpthreads/include\""
+    " ${WINSDK_INCLUDE_FLAGS}")
+
+unset(WINSDK_LIBDIR_FLAGS)
+unset(WINSDK_INCLUDE_FLAGS)
 
 unset(SOLUTION_DIR)
 

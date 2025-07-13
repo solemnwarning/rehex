761494452558b01c2a8e7f87b9092bd83938c2aa
Daniel Collins <solemnwarning@solemnwarning.net>
2025-07-13 14:57:06 +0100

    Release 0.63.1

--- msvc/version.bat
+++ msvc/version.bat
@@ -1,22 +1,7 @@
 @echo off
 
-rem This script is CALLed by other batch scripts on Windows to get the REHex version.
-rem In releases, this should be replaced with a static script like the following:
-rem
-rem set LONG_VERSION=Version 1.2.3
-rem set SHORT_VERSION=1.2.3
-rem set VERSION_WORDS=1,2,3,0
-
-pushd %~dp0
-cd ..
-
-for /f "delims=" %%i in ('git log -1 "--format=%%H"') do set GIT_COMMIT_SHA=%%i
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-set LONG_VERSION=Snapshot %GIT_COMMIT_SHA%
-set SHORT_VERSION=%GIT_COMMIT_SHA%
-set VERSION_WORDS=0,0,0,0
-
-popd
+set LONG_VERSION=Version 0.63.1
+set SHORT_VERSION=0.63.1
+set VERSION_WORDS=0,63,1,0
 
 exit /b 0

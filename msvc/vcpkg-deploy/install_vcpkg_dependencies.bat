1c399026e56bc03fa336e1b7d975a116947f001c
Daniel Collins <solemnwarning@solemnwarning.net>
2025-08-04 11:32:41 +0100

    Rework vcpkg installation process.
    This commit switches vcpkg to "manifest mode" so that we can pin
    dependencies to specific versions, as part of this, we have to also
    pin the whole vcpkg tree at a specific version, so that gets updated
    to the latest version when building a development snapshot.
    
    The vcpkg install trees are now installed under the vcpkg-deploy
    directory (per architecture) and will be cleaned like a normal VS
    project.
    
    See also:
    
    https://github.com/microsoft/vcpkg/issues/46736
    https://github.com/microsoft/vcpkg/pull/46473

--- msvc/vcpkg-deploy/install_vcpkg_dependencies.bat
+++ msvc/vcpkg-deploy/install_vcpkg_dependencies.bat
@@ -1,10 +1,24 @@
 @echo off
 
 REM get vcpkg distribution
-if not exist vcpkg git clone https://github.com/microsoft/vcpkg.git
+if not exist vcpkg git clone https://github.com/microsoft/vcpkg.git || exit /b %errorlevel%
 
 REM build vcpkg
-if not exist vcpkg\vcpkg.exe call vcpkg\bootstrap-vcpkg.bat -disableMetrics
+if not exist vcpkg\vcpkg.exe call vcpkg\bootstrap-vcpkg.bat -disableMetrics || exit /b %errorlevel%
 
-REM install required packages
-vcpkg\vcpkg.exe install --overlay-triplets="%~dp0triplets" --overlay-ports="%~dp0ports" --triplet %1 botan jansson wxwidgets capstone[arm,arm64,mips,ppc,sparc,x86] lua[tools] libiconv libunistring
+call %~dp0\..\version.bat
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+if not "%GIT_COMMIT_SHA%" == "" (
+	rem We are building a development snapshot, automatically bump the baseline version to track
+	rem the current HEAD of vcpkg, releases will remain pinned at the tag version.
+
+	pushd vcpkg || exit /b %errorlevel%
+	git pull --ff-only || exit /b %errorlevel%
+	popd || exit /b %errorlevel%
+
+	vcpkg\vcpkg.exe x-update-baseline --x-manifest-root=vcpkg-deploy || exit /b %errorlevel%
+)
+
+copy /Y vcpkg-deploy\vcpkg.json "%2\vcpkg.json" || exit /b %errorlevel%
+vcpkg\vcpkg.exe install --overlay-triplets="%~dp0triplets" --overlay-ports="%~dp0ports" --triplet %1 --x-manifest-root="%2" || exit /b %errorlevel%
